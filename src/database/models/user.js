4648581a08c44178f73eee05bdccd1c6ba7b0c73
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-07-15 19:36:18 -0300

    requisito 2 feito

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const createUserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    tableName: 'User'
+  })
+  return User;
+};
+
+module.exports = createUserModel
+
