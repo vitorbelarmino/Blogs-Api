ac887c3d1389c292725a7ff7cd7211013806012c
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-07-19 20:26:34 -0300

    requisito 7

---
+++ src/database/models/category.js
@@ -0,0 +1,26 @@
+'use strict';
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement:true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    tableName: 'Category',
+    timestamps: false,
+  })
+  return Category;
+};
