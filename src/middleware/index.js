6023349eb79c9ed98a6f48495fa1120d15a9216b
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-07-18 23:10:14 -0300

    requisito 4

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,17 +1,12 @@
 const { CustomError } = require('../helpers/CustomError');
-// const { loginSchema } = require('../schema');
+const { loginSchema } = require('../schema');
 
 const loginValidate = (req, res, next) => {
   const login = req.body;
-  console.log(login);
-  if (!login.password || !login.email) {
-    throw new CustomError(400, 'Some required fields are missing');
-  }
-  // const { error } = loginSchema.validate(login);
-  // console.log(error.message);
-  // if (error) return res.status(400).json({ message: error.message });
+  const { error } = loginSchema.validate(login);
+  if (error) throw new CustomError(400, 'Some required fields are missing');
   
-  next();
+  return next();
 };
 
 module.exports = { loginValidate };
